fa3458b4f88b6501a96271d9c7d115ebf8bab156
project-admin <790684386@qq.com>
2018-11-07 14:49:41 +0800

    update

--- src/views/Top/Top.js
+++ src/views/Top/Top.js
@@ -4,6 +4,7 @@ import {toplistDetail} from '../../request/api';
 import PublicHeader from '../../components/public/header/Header';
 import FooterComponent from '../../components/common/Footer/footer';
 import {Toast} from "antd-mobile";
+import Scroll from '../../components/public/scroll/scroll';
 
 class Top extends React.Component {
   constructor (props) {
@@ -33,28 +34,30 @@ class Top extends React.Component {
   render() {
     const {list} = this.state;
     return (
-        <div className="top">
+        <div className="topWrap">
           <PublicHeader title="排行榜" background="#d94036"/>
           <div className="topCont box1">
-            <ul className="topList">
-              {
-                list && list.map((item, index) => (
-                    <li className="topListItem flex" key={index}>
-                      <div className="left">
-                        <img src={item.coverImgUrl} alt={item.name}/>
-                        <span>{item.updateFrequency}</span>
-                      </div>
-                      <div className="right box1 flex justify-center dir-column">
-                        {
-                          item.tracks && item.tracks.map((tracks, tracksindex) => (
-                              <p key={tracksindex}>{`${tracksindex+1}. ${tracks.first} - ${tracks.second}`}</p>
-                          ))
-                        }
-                      </div>
-                    </li>
-                ))
-              }
-            </ul>
+            <Scroll data={list} scrollbar={true}>
+              <ul className="topList">
+                {
+                  list && list.map((item, index) => (
+                      <li className="topListItem flex" key={index}>
+                        <div className="left">
+                          <img src={item.coverImgUrl} alt={item.name}/>
+                          <span>{item.updateFrequency}</span>
+                        </div>
+                        <div className="right box1 flex justify-center dir-column">
+                          {
+                            item.tracks && item.tracks.map((tracks, tracksindex) => (
+                                <p key={tracksindex}>{`${tracksindex+1}. ${tracks.first} - ${tracks.second}`}</p>
+                            ))
+                          }
+                        </div>
+                      </li>
+                  ))
+                }
+              </ul>
+            </Scroll>
           </div>
           <FooterComponent/>
         </div>
