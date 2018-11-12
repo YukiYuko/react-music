5cfb93de833be582fda2377feededc4741d15457
project-admin <790684386@qq.com>
2018-11-12 10:33:20 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Switch, Route, HashRouter as Router,} from 'react-router-dom'
+import {Switch, Route, BrowserRouter as Router,} from 'react-router-dom'
 
 import Home from './views/Home/Home'
 import Person from './views/Person/Person'
@@ -11,40 +11,53 @@ import Play from './views/Play/Play'
 import Top from './views/Top/Top'
 import Subscribers from './views/Subscribers/Subscribers'
 import High from './views/Highquality/Highquality'
-import './App.css';
+import './App.less';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+const styles = {};
+
+styles.fill = {
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0
+};
 
 class App extends Component {
   render() {
     return (
         <Router>
           <Route render={({ location }) => (
-              <div className="App">
-                <TransitionGroup>
-                  {/* no different than other usage of
-                CSSTransition, just make sure to pass
-                `location` to `Switch` so it can match
-                the old location as it animates out
-            */}
-                  <CSSTransition
-                      key={location.key}
-                      classNames="fade"
-                      timeout={300}
-                  >
-                    <Switch location={location}>
-                      <Route exact path='/' component={Home}/>
-                      <Route exact path='/Video' component={Video}/>
-                      <Route exact path='/Person' component={Person}/>
-                      <Route exact path='/Friend' component={Friend}/>
-                      <Route exact path='/Setting' component={Setting}/>
-                      <Route exact path='/SongList/:id' component={SongList}/>
-                      <Route exact path='/Play/:id' component={Play}/>
-                      <Route exact path='/Top' component={Top}/>
-                      <Route exact path='/Subscribers' component={Subscribers}/>
-                      <Route exact path='/High' component={High}/>
-                    </Switch>
-                  </CSSTransition>
-                </TransitionGroup>
+              <div style={styles.fill}>
+                <Route>
+                  <div className="App">
+                    <TransitionGroup>
+                      {/* no different than other usage of
+                          CSSTransition, just make sure to pass
+                          `location` to `Switch` so it can match
+                          the old location as it animates out
+                      */}
+                      <CSSTransition
+                          key={location.key}
+                          classNames="fade"
+                          timeout={300}
+                      >
+                        <Switch location={location}>
+                          <Route exact path='/' component={Home}/>
+                          <Route exact path='/Video' component={Video}/>
+                          <Route exact path='/Person' component={Person}/>
+                          <Route exact path='/Friend' component={Friend}/>
+                          <Route exact path='/Setting' component={Setting}/>
+                          <Route exact path='/SongList/:id' component={SongList}/>
+                          <Route exact path='/Play/:id' component={Play}/>
+                          <Route exact path='/Top' component={Top}/>
+                          <Route exact path='/Subscribers' component={Subscribers}/>
+                          <Route exact path='/High' component={High}/>
+                        </Switch>
+                      </CSSTransition>
+                    </TransitionGroup>
+                  </div>
+                </Route>
               </div>
           )}/>
         </Router>
