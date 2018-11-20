9c4a31536e3894967b67935838076eb1709a878d
project-admin <790684386@qq.com>
2018-11-20 19:21:46 +0800

    update

--- src/views/Setting/Setting.js
+++ src/views/Setting/Setting.js
@@ -2,15 +2,49 @@ import React from 'react'
 import './Setting.less'
 import { inject, observer } from 'mobx-react';
 import PublicHeader from "../../components/public/header/Header";
+import {getUserInfo} from '../../request/api';
+import localforage from "localforage";
+import {Toast} from "antd-mobile";
+import stores from "../../store";
 
 @inject('user')
 @observer
 class Setting extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      userInfo: ''
+    }
+  }
   componentDidMount() {
-    console.log(this.props)
+    localforage.getItem('userInfo').then((value)=>{
+      // 当离线仓库中的值被载入时，此处代码运行
+      if (value) {
+        stores.user.changeUser(value);
+        this._getUserInfo();
+      }
+    }).catch(function(err) {
+      // 当出错时，此处代码运行
+      console.log(err);
+    });
+  }
+  _getUserInfo () {
+    if (!this.props.user.userInfo.userId) {
+      return
+    }
+    getUserInfo({uid: this.props.user.userInfo.userId}).then((res) => {
+      if (res.code === 200) {
+        delete res.code;
+        this.setState({
+          userInfo: res
+        })
+      }else {
+        Toast.fail(res.msg, 2);
+      }
+    })
   }
   render() {
-    const {userInfo} = this.props.user;
+    const {userInfo} = this.state;
     return (
       <div className="setting inner-warp">
         <PublicHeader showLeft={false} title="账号"/>
@@ -18,19 +52,24 @@ class Setting extends React.Component {
           <div className="setting-head-top flex justify-between">
             <div className="left flex items-center">
               <div className="avatar">
-                <img src={userInfo.avatarUrl} alt={userInfo.nickname}/>
+                <img src={userInfo.profile && userInfo.profile.avatarUrl} alt={userInfo.profile && userInfo.profile.nickname}/>
               </div>
               <div className="nickname">
-                <p>{userInfo.nickname}</p>
-                <a className="border-radius">Lv.7</a>
+                <p>{userInfo.profile && userInfo.profile.nickname}</p>
+                <div className="signature">{userInfo.profile && userInfo.profile.signature}</div>
+                <a className="border-radius">Lv.{userInfo.level}</a>
               </div>
             </div>
             <div className="right flex items-center">
-              <a className="border-radius">已签到 ></a>
+              {
+                userInfo.mobileSign ?
+                    <a className="border-radius">已签到 ></a> :
+                    <a className="border-radius noSign">签到 ></a>
+              }
             </div>
           </div>
-          <div className="setting-head-bottom">
-
+          <div className="setting-head-bottom flex">
+            <div className="setting-menu"></div>
           </div>
         </div>
       </div>
