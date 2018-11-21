68eb389e121a2de2ab21e03d9e4c677890cb952e
project-admin <790684386@qq.com>
2018-11-21 17:36:57 +0800

    update

--- src/components/common/Search/Search.js
+++ src/components/common/Search/Search.js
@@ -1,24 +1,104 @@
 import React from 'react'
 import './search.less'
-import { SearchBar } from 'antd-mobile';
+import {SearchBar, Toast} from 'antd-mobile';
+import {searchHot, search} from '../../../request/api';
+import localforage from "localforage";
+import {CSSTransition} from "react-transition-group";
 
 class Search extends React.Component {
-  componentDidMount() {}
+  componentDidMount() {
+    // this.getHot();
+    // this.getSearch();
+  }
   state = {
-    value: '美食',
+    value: '许嵩',
+    hot: '',
+    show: false,
+    type: 1,
+    offset: 0,
+    list: []
   };
   onChange = (value) => {
     this.setState({ value });
   };
+  onShow = (key, value = true) => () =>{
+    this.setState({ [key]: value });
+  };
+  // 获取热门
+  getHot () {
+    searchHot().then((res) => {
+      if (res.code === 200) {
+        this.setState({
+          hot: res.result
+        })
+      }else {
+        Toast.fail(res.msg, 2);
+      }
+    })
+  }
+  // 搜索
+  getSearch () {
+    let params = {
+      keywords: this.state.value,
+      type: this.state.type,
+      offset: this.state.offset
+    };
+    search(params).then((res) => {
+      if (res.code === 200) {
+        // this.setState({
+        //   hot: res.result
+        // })
+        console.log(res)
+      }else {
+        Toast.fail(res.msg, 2);
+      }
+    })
+  }
+
   render() {
+    const {hot, show} = this.state;
     return (
-        <div className="search">
-          <SearchBar placeholder="Search" maxLength={8}
-                     value={this.state.value}
-                     onSubmit={value => console.log(value, 'onSubmit')}
-                     onClear={value => console.log(value, 'onClear')}
-                     onChange={this.onChange}/>
-        </div>
+      <div className="search">
+        <SearchBar placeholder="Search" maxLength={8}
+                   value={this.state.value}
+                   onSubmit={value => console.log(value, 'onSubmit')}
+                   onClear={this.onShow('show', false)}
+                   onFocus={this.onShow('show')}
+                   onChange={this.onChange}/>
+        <CSSTransition
+            in={show}
+            timeout={300}
+            classNames="fade"
+            unmountOnExit
+        >
+          <div className="searchBox">
+            {/*搜索相关*/}
+            <div className="searchMess">
+              <div className="singer-cat">
+                <i className="iconfont icon-geshou"/>
+                <span>歌手分类 ></span>
+              </div>
+              <div className="search-hot">
+                <div className="search-hot-title">热门搜索</div>
+                <div className="search-hot-list flex wrap-wrap">
+                  {
+                    hot && hot.hots.map((item, index) => (
+                        <a key={index}>{item.first}</a>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+
+            {/*搜索结果*/}
+            <div className="searchList">
+              <div className="searchListType">
+
+              </div>
+            </div>
+          </div>
+        </CSSTransition>
+      </div>
     )
   }
 }
--- src/request/api.js
+++ src/request/api.js
@@ -171,3 +171,29 @@ export const loginPhone = (params) => api.get(`${BASE_URL}/login/cellphone`, par
 * 调用例子 : /user/detail?uid=32953014
 * */
 export const getUserInfo = (params) => api.get(`${BASE_URL}/user/detail`, params);
+
+/*
+* 热搜
+* 说明 : 调用此接口,可获取热门搜索列表
+* 接口地址 : /search/hot
+* 调用例子 : /search/hot
+* */
+export const searchHot = (params) => api.get(`${BASE_URL}/search/hot`, params);
+/*
+* 搜索
+* 说明 : 调用此接口 , 传入搜索关键词可以搜索该音乐 / 专辑 / 歌手 / 歌单 / 用户 ,
+* 关键词可以多个 , 以空格隔开 , 如 " 周杰伦 搁浅 "( 不需要登录 ),
+* 搜索获取的 mp3url 不能直接用 , 可通过 /song/url 接口传入歌曲 id
+* 获取具体的播放链接
+*
+* 必选参数 : keywords : 关键词
+* 可选参数 : limit : 返回数量 , 默认为 30 offset : 偏移数量，用于分页 ,
+* 如 : 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+* type: 搜索类型；默认为 1 即单曲 , 取值意义 : 1: 单曲, 10: 专辑,
+* 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV,
+* 1006: 歌词, 1009: 电台, 1014: 视频
+
+* 接口地址 : /search
+* 调用例子 : /search?keywords= 海阔天空
+* */
+export const search = (params) => api.get(`${BASE_URL}/search`, params);
